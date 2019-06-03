3c832fae7eb7666f5c6aa210445f5c4829df5d91
enivaldo bonelli <45775341+fisicaetcetera@users.noreply.github.com>
2019-06-02 21:35:52 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,38 +1,67 @@
-//let cam;
 let earthjpg;
 let isspng;
 let starsjpg;
 let assinatura;
 let posx=-200, posz=100;
 let zsol;
+let ysol;
 let xsol;
+let sollat, sollong;
+let isslong, isslat;
+const earthradius = 180;
+const moonradius = earthradius/3;
+const moondistance = earthradius * 50;
+const isssize = earthradius/2;
+let piover2;
+let pi;
+
 
 function preload() {
   earthjpg = loadImage('earthmap1k.jpg');
   starsjpg = loadImage('stars.jpg');
   isspng = loadImage('iss.png');
 }
-
 function setup() {
-  createCanvas(1400,600, WEBGL);
+  pi = PI;
+  piover2 = PI/2;
+  createCanvas(1400,700, WEBGL);
+  const degtorad = PI/180; 
+    const api_url = 'https://api.wheretheiss.at/v1/satellites/25544';
+    var myVar = setInterval(getISS, 5000);
+    async function getISS(){
+      const response = await fetch(api_url);
+      const data = await response.json();
+      isslat = data.latitude * degtorad;
+      isslong = data.longitude * degtorad;
+      sollat = data.solar_lat * degtorad;
+      sollong = data.solar_lon * degtorad;
+
+      //console.log(isslat);
+      //console.log(isslong);
+
+    };
+
   assinatura = createGraphics(600, 150);
   assinatura.background(255, 100);
   assinatura.fill(0);
   assinatura.textAlign(CENTER);
   assinatura.textSize(75);
-  assinatura.text('Bonelli_1905', 280, 70);
+  assinatura.text('Bonelli_1906', 280, 70);
 
 }
 
 function draw() {
   background(0);
   translate(0,0,mouseX/5);
+  
+  //ESTRELAS
   //push();
   //translate(0, 0, -1000);
   //texture(starsjpg);
   //plane(2500);
   //pop();
 
+  //ASSINATURA
   push();
   texture(assinatura);
   translate(posx++,50,posz--);
@@ -40,26 +69,38 @@ function draw() {
   plane(200, 80);
   pop();
   
-  zsol = sin(millis()/10000);
-  xsol = cos(millis()/10000);
-  directionalLight(255, 255, 255, -xsol, 0, -zsol)
-  pointLight(0,0,205,0,0,395);
-
+  //LUZ SOLAR
+  //zsol = sin(millis()/1000);
+  //xsol = cos(millis()/1000);
+  xsol = cos(-sollat+isslat)*sin(-sollong+isslong);
+  zsol = cos(-sollat+isslat)*cos(-sollong+isslong);
+  ysol = sin(-sollat+isslat);
+  directionalLight(255, 255, 255, xsol, ysol, -zsol)
+  directionalLight(60,60,60,0,0,-1);
+  
+  //TERRA
   push();
-
   texture(earthjpg);
-  rotateY(-frameCount / 2280);
-  sphere(250);
-    
+  rotateY(pi-isslong);
+  rotateX(isslat);
+  sphere(earthradius);
   pop();
   
+  //ESTAÇÃO
   push();
-  translate(0, 0, 250 + 25 );
+  translate(0, 0, earthradius + earthradius/10 );
   texture(isspng);
-  plane(100);
+  plane(isssize);
   pop();
 
-  
+   //LUA
+  push();
+  //translate(0,0,moondistance/100);
+  noStroke();
+  rotateY(millis()/1000000);
+  translate(0,0,-earthradius*50);
+  sphere(moonradius);
+  pop();
  
 
 
